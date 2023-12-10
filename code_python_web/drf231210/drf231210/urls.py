df686adb48d095f228ce0513958e8d2fb682d545
time1043 <1665434994@qq.com>
2023-12-10 21:52:19 +0800

    231210第五次21

---
+++ code_python_web/drf231210/drf231210/urls.py
@@ -0,0 +1,24 @@
+"""drf231210 URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from app01 import views
+
+urlpatterns = [
+    # 路由含有动态url -> 视图函数(入参)
+    path('user/<int:pk>/', views.UserView.as_view()),  # django的CBV
+    path('info/<str:dt>/', views.InfoView.as_view()),  # drf的CBV
+]
